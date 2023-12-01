a17fdd7c4dcc30473078f8c9c3e9e2c7dad9f5f4
Punny <contactme@gopunny.com>
2023-12-01 23:29:57 +0800

    打包

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import QuestionTypeView from '@/components/QuestionTypeView.vue'
 import QuestionView from '@/components/QuestionView.vue'
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {path:'/QuestionType',component: QuestionTypeView},
     {path:'/Question/:id',component: QuestionView},
